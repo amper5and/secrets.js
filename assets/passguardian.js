e882c1f8a94ffc4d047fa50a4e7478022a77bc29
AXS <alexstets@gmail.com>
2013-02-07 18:54:59 -0500

    support v0.1.6 of secrets.js

--- assets/passguardian.js
+++ assets/passguardian.js
@@ -1,9 +1,8 @@
 secrets.setRNG(null, false);
-var hexChars = '0123456789abcdefABCDEF';
-var decChars = '0123456789';
+var pw = 'PassGuardian';
 
 function error(location, message, hide){
-	$(location).before('<div class="alert alert-error fade in"><button type="button" class="close" data-dismiss="alert">&times;</button><h4>Error</h4>'+message+'</div>')
+	$($(location).parent()).before('<div class="alert alert-error fade in popupError"><button type="button" class="close" data-dismiss="alert">&times;</button><h4>Error</h4>'+message+'</div>')
 	if(hide){
 		$(hide).hide();
 	}
@@ -12,14 +11,13 @@ $(document).on('click', '#splitButton', function(ev){
 	if(secrets.getConfig().bits !== 8){
 		secrets.init(8);
 	}
-	$(this).prevAll('.alert').remove();
+	$('#split-tab .popupError').remove();
 	
 	var string = $('#string').val();
 	if(string === ''){
 		return error(this, 'Input cannot be empty.', '#split-result')
 	}
 	var type = $('.inputType.active').attr('data-inputType');
-	
 	var numShares = $('#numShares').val() * 1;
 	if(typeof numShares !== 'number' || isNaN(numShares) || numShares < 2 || numShares > 255){
 		return error(this, 'Number of shares must be an integer between 2 and 255, inclusive.', '#split-result')
@@ -29,28 +27,14 @@ $(document).on('click', '#splitButton', function(ev){
 		return error(this, 'Threshold must be an integer between 2 and 255, inclusive.', '#split-result')
 	}
 	
-	var radix = 16;
 	if(type==='text'){
-		string = secrets.toHex(string);
-	}else if(type==='hex'){
-		for(var i=0, len=string.length; i<len; i++){
-			if(!~hexChars.indexOf(string[i])){
-				return error(this, 'Input contains at least one invalid hex character: '+string[i], '#split-result')
-			}
-		}
-	}else if(type==='dec'){
-		for(var i=0, len=string.length; i<len; i++){
-			if(!~decChars.indexOf(string[i])){
-				return error(this, 'Input contains at least one invalid decimal character: '+string[i], '#split-result')
-			}
-		}
-		radix = 10;
+		string = secrets.str2hex(string);
 	}
 	if(!$('#shares').length){
 		$(this).parent().after('<div id="split-result" style="display:none;" class="alert alert-block alert-success fade in"><button type="button" class="close" data-dismiss="alert">&times;</button><h4>Secret shares</h4>One share per line<pre id="shares"></pre></div>');
 	}
 	try{
-		var shares = secrets.share(string, numShares, threshold, radix);
+		var shares = secrets.share(string, numShares, threshold);
 		var textarea = $('#shares');
 		shares = shares.join('<br>');
 		textarea.html(shares);
@@ -63,8 +47,7 @@ $(document).on('click', '#splitButton', function(ev){
 })
 
 $(document).on('click', '#reconButton', function(ev){
-	$(this).prevAll('.alert').remove();
-	
+	$('#recon-tab .popupError').remove();
 	var shares = [];
 	$('.shareInput').each(function(){
 		var share = $.trim($(this).val());
@@ -79,20 +62,16 @@ $(document).on('click', '#reconButton', function(ev){
 	}
 	var type = $('.reconType.active').attr('data-inputType');
 	
-	var radix = 16;
-	if(type==='dec'){
-		radix = 10;
-	}
 	if(!$('#reconstruction').length){
 		$(this).parent().after('<div id="recon-result" style="display:none;" class="alert alert-block alert-success fade in"><button type="button" class="close" data-dismiss="alert">&times;</button><h4>Reconstructed secret</h4><pre id="reconstruction"></pre></div>');
 	}
 	try{
-		var recon = secrets.combine(shares,null,radix);
+		var recon = secrets.combine(shares);
 		if(type === 'text'){
-			recon = secrets.toString(recon);
+			recon = secrets.hex2str(recon);
 		}
 		$('#reconstruction').text(recon);
-		$('#recon-result').show()
+		$('#recon-result').show();
 	}catch(e){
 		return error(this, 'Reconstruction ' + e, '#recon-result')
 	}
@@ -112,9 +91,9 @@ $(document).on('click','#clearAllButton', function(ev){
 	})
 })
 
-$(document).on('click','.inputType[data-inputType=dec],.inputType[data-inputType=hex]', function(ev){
+$(document).on('click','.inputType[data-inputType=hex]', function(ev){
 	var string = $('#string');
-	var val = string.val().replace(/\n/g,'');
+	var val = string.val().replace(/\n/g,' ');
 	if(string.is('textarea')){
 		string.replaceWith('<input class="input-block-level" id="string" type="text" placeholder="Secret to share" value="'+val+'">')
 	}
@@ -125,4 +104,40 @@ $(document).on('click','.inputType[data-inputType=text]', function(ev){
 	if(string.is('input')){
 		string.replaceWith('<textarea class="input-block-level" id="string" type="text" rows="3" placeholder="Secret to share">'+val+'</textarea>')
 	}
+})
+
+$(document).on('click','#resetSplitForm', function(ev){
+	$('#split-tab .popupError').remove();
+	$('#string').val('');
+	$('#numShares').val(2);
+	$('#threshold').val(2);
+	var activeType = $('.inputType.active');
+	if(!activeType.attr('text')){
+		activeType.removeClass('active');
+		$('.inputType[data-inputType=text]').addClass('active')
+	}
+	$('#split-result').hide();
+	$('#shares').empty();
+})
+
+$(document).on('click','#resetReconForm', function(ev){
+	$('#recon-tab .popupError').remove();
+	$('.shareInput').each(function(){
+		if($('.shareInput').length >=3 ){
+			$(this).remove()
+		}else{
+			$(this).val('');
+		}
+	})
+	$('#string').val('');
+	$('#numShares').val(2);
+	$('#threshold').val(2);
+	var activeType = $('.reconType.active');
+	if(!activeType.attr('text')){
+		activeType.removeClass('active');
+		$('.reconType[data-inputType=text]').addClass('active')
+	}
+	$('#recon-result').hide();
+	$('#reconstruction').empty();
+	$('#recon-hmac').empty();
 })
